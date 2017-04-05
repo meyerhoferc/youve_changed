7c426b59093a938d5e20fdf0db5cc08acd36c854
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-04-05 09:17:18 -0600

    Setup routes and controller.

---
+++ app/controllers/notification_controller.rb
@@ -0,0 +1,7 @@
+class NotificationController < ApplicationController
+  def create
+    FriendNotifier.inform(current_user, params[:email]).deliver_now
+    flash[:notice] = "Successfully told your friend that they've changed"
+    redirect_to root_url
+  end
+end
